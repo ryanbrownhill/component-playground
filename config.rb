f8e79e35f7cc88cf5c331c844a2e56ea170b130d
ryanbrownhill <ryanbrownhill@gmail.com>
2015-05-16 15:07:01 -0500

    Refining Layout

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Compass CSS framework config file
+# require 'susy'
+# require 'modular-scale'
+# require 'sassy-buttons'
+# require 'breakpoint'
+# require 'compass-css-arrow'
+# require 'rgbapng'
+# Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "scss"
+css_dir = "dist/css"
+images_dir = "img"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
--- js/animation.js
+++ js/animation.js
@@ -1,25 +1,23 @@
 // window.onbeforeunload = function (e) {
 //     document.querySelectorAll('.box1').className = 'fadein';
 // }
-// 
-// http://www.onextrapixel.com/2010/02/23/how-to-use-jquery-to-make-slick-page-transitions/
-// 
+
+/// adding active class http://jsfiddle.net/chipChocolate/pb95kv32/
+
 console.log("test");
 
 //INDEX JS
 
-// var box1 = document.getElementById("box1");
+var box1 = document.getElementById("box1");
 
-// box1.onclick = function () {
-// 	console.log("test2");
-//  if (classie.has(box1,"fadeinup")) {
-//     classie.remove(box1,"fadeinup");
-// } else {
-//     classie.add(box1,"fadeinup");
-// }
-// };
-
-///
+box1.onclick = function () {
+	console.log("test2");
+ if (classie.has(box1,"fadeinup")) {
+    classie.remove(box1,"fadeinup");
+} else {
+    classie.add(box1,"fadeinup");
+}
+};
 
 var navItems = document.querySelectorAll("ul li");
 var slider = document.querySelectorAll(".slider");
